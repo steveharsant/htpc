1a67ef6071d67a8585108c3360b187863791afd8
steveharsant <38461965+steveharsant@users.noreply.github.com>
2019-04-25 12:56:30 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+#===================================================#
+#                  DIY HTPC Installer               #
+#               Author: Steven Harsant              #
+#                  Date: 25/04/2019                 #
+#                   Version: 0.1                    #
+#===================================================#
+#                                                   #
+#      Rough and raw installer script to setup      #
+#       your very own DIY HTPC based on Xlunch      #
+#      and Ubuntu. Extra packages include kodi,     #
+#     Chrome, Budgie Desktop Environment, Spotfy    #
+# and a bunch of other bits to glue it all together #
+#                                                   #
+#   Credits to Tomas-M and the hard work that went  #
+#     into xlunch (https://github.com/Tomas-M)      #
+#                                                   #
+#===================================================#
+
+
+#################
+# Set Variables #
+#################
+WHITE='\033[1;37m'
+RED='\033[0;91m'
+GREEN='\33[92m'
+YELLOW='\033[93m'
+BLUE='\033[1;34m'
+
+FAIL=${RED}'FAIL:'${WHITE} #FAIL MESSAGES
+PASS=${GREEN}'PASS:'${WHITE} #PASS MESSAGES
+INFO=${YELLOW}'INFO:'${WHITE} #INFO MESSAGES
+HINT=${BLUE}'HINT:'${WHITE} #HINT MESSAGES
+
+# Get logged in username
+USERNAME=`awk -F'[/:]' '{if ($3 >= 1000 && $3 != 65534) print $1}' /etc/passwd | head -n 1` #Logged in username
+
+###############################
+# Check Script Is Run As root #
+###############################
+if [ "$EUID" -ne 0 ]
+  then
+    printf "${FAIL} Permission Denied \n"
+    printf "${INFO} Run with the sudo command \n"
+    printf "${HINT} Try the command: ${YELLOW}sudo !! ${WHITE}\n"
+    exit
+  else
+    printf "${PASS} Script running with super user rights \n"
+fi
+
+########################
+# Install Dependancies #
+########################
+# Add repositories
+printf "${INFO} Adding Budgie Desktop Environment repository \n"
+add-apt-repository -y ppa:ubuntubudgie/backports > /dev/null 2>&1  #Budgie Backports Repo For Applets
+printf "${INFO} Adding Google Chrome repository \n"
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | tee /etc/apt/sources.list.d/google-chrome.list > /dev/null 2>&1 #chrome Web Browser
+printf "${INFO} Adding Spotify repository \n"
+echo deb http://repository.spotify.com stable non-free | tee /etc/apt/sources.list.d/spotify.list > /dev/null 2>&1 #Spotify Music Player
+
+printf "${INFO} Running apt update \n"
+apt-get update -qq
+
+printf "${INFO} Installing dependancies and packages \n"
+apt install -qq budgie-core budgie-showtime-applet gcc google-chrome-stable kodi libimlib2-dev libx11-dev make samba spotify-client -y > /dev/null 2>&1
+
+##################
+# Install Xlunch #
+##################
+mkdir ~/xlunch
+cd ~/xlunch
+printf "${INFO} Downloading xlunch \n"
+wget https://github.com/Tomas-M/xlunch/archive/v4.4.0.tar.gz
+tar -xzvf v4.4.0.tar.gz
+rm v4.4.0.tar.gz
+cd xlunch-4.4.0
+printf "${INFO} installing xlunch \n"
+make install
+cd ~/xlunch
+rm -rf xlunch-4.4.0/
+
+####################
+# Configure Xlunch #
+####################
+printf "${INFO} Downloading xlunch HTPC configuration and resources \n"
+wget https://github.com/steveharsant/htpc/archive/master.zip
+unzip master.zip
+cp -r ./htpc-master/* .
+rm master.zip
+chmod +x run.sh
+
+printf "${INFO} Setting default background \n"
+gsettings set org.gnome.desktop.background picture-uri "file:///home/${UNAME}/xlunch/images/wallpapers/wallpaper.png"
+
+printf "${INFO} Creating autostart entry for xlunch \n"
+cat > /home/${UNAME}/.config/autostart/xlunch.desktop << EOL
+[Desktop Entry]
+Type=Application
+Name=xlunch launcher
+Description=xlunch launcher
+Exec=/home/${UNAME}/xlunch/run.sh
+EOL
